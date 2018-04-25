13b6b8ec13bdc53764d0e06e0366701b57a1737a
Андрей Ивойлов <a.ivoylov@bars-open.ru>
2018-04-25 23:44:09 +0300

    added eap example project

---
+++ WorkerAsyncEAP.NET2/Worker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using Helpers;
+
+namespace WorkerAsyncEAP.NET2
+{
+    public delegate void WorkCompletedEventHandler(object sender, AsyncCompletedEventArgs args);
+
+    internal class Worker
+    {
+        private delegate void WorkerEventHandler(AsyncOperation asyncOp);
+
+        private readonly SendOrPostCallback _onCompletedDelegate;
+        
+        public event WorkCompletedEventHandler WorkCompleted;
+
+        public Worker()
+        {
+            _onCompletedDelegate = new SendOrPostCallback(CompletedDelegateFunc);
+        }
+        
+        private void CompletedDelegateFunc(object operationState)
+        {
+            AsyncCompletedEventArgs e = operationState as AsyncCompletedEventArgs;
+
+            if (WorkCompleted != null)
+            {
+                WorkCompleted(this, e);
+            }
+        }
+
+        public void DoWork(object userState)
+        {
+            AsyncOperation asyncOp = AsyncOperationManager.CreateOperation(userState);
+            
+            Console.WriteLine("Начало работы");
+            WorkerEventHandler workInternal = new WorkerEventHandler(LongOperation);
+            workInternal.BeginInvoke(asyncOp, null, null);
+        }
+        
+        private void LongOperation(AsyncOperation asyncOp)
+        {
+            Console.WriteLine("Работаю...");
+            Thread.Sleep(Helper.WorkDelay);
+
+            AsyncCompletedEventArgs e = new AsyncCompletedEventArgs(null, false, asyncOp.UserSuppliedState);
+            
+            Console.WriteLine();
+            Console.WriteLine("Работа завершена");
+            
+            asyncOp.PostOperationCompleted(_onCompletedDelegate, e);
+        }
+    }
+}
